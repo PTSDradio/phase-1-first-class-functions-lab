23f6d490751c8f8d4ed0a5503605c18145d43db2
Lawrence Nigel Anderson <nigel.lawrenceanderson@gmail.com>
2023-06-13 15:12:46 -0500

    head hurt

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = (n) => {
+ return n.slice(0,2)
+}
+
+const returnLastTwoDrivers = (l) => {
+return l.slice(2,4)
+}
+
+const selectingDrivers= [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(n) {
+   return () => { return n*n};
+}
+
+const fareDoubler = (createFareMultiplier) => {
+    return createFareMultiplier*2
+}
+
+const fareTripler = (createFareMultiplier) => {
+    return createFareMultiplier*3
+}
+
+function selectDifferentDrivers(n ,a) {
+    return a(n)
+}
